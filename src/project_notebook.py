bb06f86cc26e6904c2502219ac5dc4b32a4df827
Zarkos99 <kzarvis15@gmail.com>
2024-06-03 22:49:06 -0400

    Initial setup and Jupytext synchronization

---
+++ src/project_notebook.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+print("Hello. This will be a great project.")
+
