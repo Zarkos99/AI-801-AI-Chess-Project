183fdbd49621cdf308ffe953bf44bcad6c2a3332
jason-schar <jason.schar@yahoo.com>
2024-06-19 23:37:40 -0400

    Update State.py

--- src/State.py
+++ src/State.py
@@ -1,7 +1,7 @@
 
-from chess_enums import Space
-from chess_enums import Player
-from chess_enums import Piece
+from ChessEnums import Space
+from ChessEnums import Player
+from ChessEnums import Piece
 
 
 class State:
