5cb0875490466b0feb9a72e9c1a7ac2bc37b66c2
jason120387 <30678390+jason120387@users.noreply.github.com>
2024-07-02 16:24:48 -0400

    Implemented Search
    -Added Search algorithm, leveraging Nodes and PathCosts

--- src/PathCost.py
+++ src/PathCost.py
@@ -17,37 +17,34 @@ class PathCost:
             match piece:
                 case Piece.W_P:
                     w_pieces += 1
-                    break
+                    
                 case Piece.W_N:
                     w_pieces += 2
-                    break
+                    
                 case Piece.W_B:
                     w_pieces += 3
-                    break
+                    
                 case Piece.W_R:
                     w_pieces += 5
-                    break
+                    
                 case Piece.W_Q:
                     w_pieces += 9
-                    break
+                    
                 case Piece.B_P:
                     b_pieces += 1
-                    break
+                    
                 case Piece.B_N:
                     b_pieces += 2
-                    break
+
                 case Piece.B_B:
                     b_pieces += 3
-                    break
+                    
                 case Piece.B_R:
                     b_pieces += 5
-                    break
+                    
                 case Piece.B_Q:
                     b_pieces += 9
-                    break
-                case _:
-                    break
-
+                    
         self.piece_weight = (
             w_pieces / b_pieces) if player == Player.White else (b_pieces / w_pieces)
         self.child_count = 0
@@ -58,5 +55,5 @@ class PathCost:
         if not self.expanded:
             return self.piece_weight
         if self.child_count == 0:
-            return 0
+            return 0.0
         return 1.0 / self.min_child_cost
