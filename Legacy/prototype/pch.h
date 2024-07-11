9a710b0718df3fd06ecb7cb51dba983604534fc2
Zarkos99 <kzarvis15@gmail.com>
2024-07-11 18:59:19 -0400

    Initial python-chess integration commit

---
+++ Legacy/prototype/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <optional>
+#include <queue>
+#include <set>
+#include <vector>
