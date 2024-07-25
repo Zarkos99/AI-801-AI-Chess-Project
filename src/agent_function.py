13b49e7e2c04bfa25206db8fd84aff07624915a9
Bennett Schoonerman <bennett411@gmail.com>
2024-07-24 22:34:57 -0400

    Use fen as key to table

--- src/agent_function.py
+++ src/agent_function.py
@@ -1,29 +1,25 @@
 """Module providing the AgentFunction class."""
 
-from dataclasses import dataclass, field
-from typing import Tuple, Dict
+from typing import Dict
+from chess_puzzle_data import obtain_latest_daily_puzzle, ChessPuzzle
 
-from chess import Board
-from action import Action
-from percept_sequence import PerceptSequence
-from chess_puzzle_data import obtain_latest_daily_puzzle
-
-@dataclass
 class AgentFunction:
     """Class representing an agent function, which is an abstract mathematical description of an
        agent's behavior that maps any given percept sequence to an action."""
-    partial_table: Dict[Tuple[str, ...], Action] = field(default_factory=dict)
 
-    def __call__(self, percept_sequence: PerceptSequence) -> Action:
-        return self.partial_table.get(percept_sequence, "No action found")
+    def __init__(self, puzzle: ChessPuzzle) -> None:
+        self.partial_table: Dict[str, str] = {}
 
-    @classmethod
-    def processGame(cls, game):
-        partial_table: Dict[Tuple[str, ...], Action] = {}
+    def __call__(self, fen_state: str) -> str:
+        print(self.partial_table)
+        return self.partial_table.get(fen_state, "No action found")
 
-        board = game.board()
+    def process_game(self, puzzle: ChessPuzzle):
+        partial_table: Dict[str, str] = {}
+
+        board = puzzle.game.board()
         percept_sequence = []
-        node = game
+        node = puzzle.game
 
         while node.variations:
             next_node = node.variation(0)
@@ -33,20 +29,19 @@ class AgentFunction:
 
             if len(percept_sequence) > 1:  # Ensure we have a previous percept to map the action to
                 # The percept sequence up to the current move maps to the current move (action)
-                partial_table[tuple(percept_sequence[:-1])] = percept_sequence[-1]
+                partial_table[board.fen()] = percept_sequence[-1]
 
             node = next_node
 
-        # Am I doing somethign wrong here? I feel like this should not be needed
-        if not hasattr(cls, 'partial_table'):
-            cls.partial_table = {}
-        cls.partial_table.update(partial_table)
-
+        self.partial_table.update(partial_table)
 
 # Use Kostis daily puzzle data parsed game
-AgentFunction.processGame(obtain_latest_daily_puzzle().game)
-agent = AgentFunction()
+puzzleOTD = obtain_latest_daily_puzzle()
+agentAction = AgentFunction(puzzleOTD)
+agentAction.process_game(puzzleOTD)
 
-percept_sequence = ("e4", "e5", "Nf3")  # Use a tuple has to be hashable and a list does not work...
-action = agent(percept_sequence)
+print(agentAction)
+
+fen_state = puzzleOTD.game.board().fen()
+action = agentAction(fen_state)
 print(action)
